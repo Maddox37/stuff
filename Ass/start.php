5ad10f867da9e549c35e77a7ed4e18d6e4476912
Maddox37 <pratasfpopras@gmail.com>
2025-04-03 16:22:16 -0500

    Add files via upload

---
+++ Ass/start.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html> 
+<html> <!--writed by encoredRain-->
+    <head>
+        <meta charset="UTF-8" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="U-AX-Compatible">
+        <link href="External/miaw.css" rel="stylesheet">
+    </head>
+    <body>
+    <div class="topbar">
+    <ul style="display: flex;gap:30px;">
+    <img style="width: 20px;" src="https://i.namu.wiki/i/J0WEtdYgIiL1hE0D05x2mBhlmVC3zlP0QqybLSqkeE4-gAg4nwE3rEvteFV3XosoKT9U_xWuCitH4m1BAdY4Gw.gif">
+       <a style="color:rgb(240, 167, 167);font-size:22px; text-decoration:none;" href="">FreakyWorld</a>
+    </ul>
+    <ul style="display: flex;gap: 20px;">
+       <a href="">FIGURAS</a>
+       <a href="">NOSOTROS</a>
+    </ul>
+</div>
+
+<div style =" margin-top: 90px;" class="main">
+   
+        <h2>PROPOSITO</h2><br>
+        <h3>en esta pagina podras encontrar una gran variedad de producos
+            relacionados con la cultura POP y anime, ademas de algunas noticias
+            y otras tematicas.
+        </h3>
+    </div>
+   
+    <div style="color: rgb(191, 255, 250);" class="main">
+        <h2>CONTENIDO</h2><br>
+        <img style="width: 240px;" src="https://cf.geekdo-images.com/eYFJLBu_tt3Mc3MRJaAFzQ__original/img/YDbZdpf57mkEhpq_q6y6VyWgudA=/0x0/filters:format(png)/pic7319987.png">
+        <h3>Las mas grandes tematicas del anime y de la cultura pop ademas de
+            figuras y contenido parecido, en esta pagina podras indagar hacerca del
+            contenido mas relevante de esta cultura MASIVA y de sus acciones mas
+            relevantes en la industria
+        </h3>
+    </div>
+     <div style="color: rgb(191, 255, 250);" class="main">
+       
+    </div>
+    </body>
+</html>
+<!--- <div style="color: rgb(191, 255, 250);" class="main">
+       
+    </div> div template-->
